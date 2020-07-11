0c81da12875f145347c6036348bc801a6dd761c1
Lucas Duarte <lucas.inacio@corenmg.gov.br>
2020-07-11 18:30:22 -0300

    NEW Email Message HTML

--- src/main/java/com/lucasduarte/lojavirtual/service/AbstractEmailService.java
+++ src/main/java/com/lucasduarte/lojavirtual/service/AbstractEmailService.java
@@ -1,16 +1,29 @@
 package com.lucasduarte.lojavirtual.service;
 
 import com.lucasduarte.lojavirtual.domain.Pedido;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
-import sun.java2d.pipe.SpanShapeRenderer;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.Date;
 
 public abstract class AbstractEmailService implements EmailService{
 
     @Value("${default.sender}")
     private String sender;
+
+    @Autowired
+    TemplateEngine templateEngine;
+
+    @Autowired
+    JavaMailSender javaMailSender;
 
     @Override
     public void sendOrderConfirmationEmail(Pedido obj){
@@ -27,4 +40,34 @@ public abstract class AbstractEmailService implements EmailService{
         sm.setText(obj.toString());
         return sm;
     }
+
+    protected String htmlFromTemplatePedido(Pedido obj){
+        Context context =  new Context();
+        context.setVariable("pedido", obj);
+        return templateEngine.process("email/confirmacaoPedido", context);
+    }
+
+    @Override
+    public void sendOrderConfirmationHtmlEmail(Pedido obj){
+        try {
+            MimeMessage mm = prepareMimeMessageFromPedido(obj);
+            sendHtmlEmail(mm);
+        }
+        catch (MessagingException e){
+            sendOrderConfirmationHtmlEmail(obj);
+        }
+    }
+
+    protected  MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
+        mmh.setTo(obj.getCliente().getEmail());
+        mmh.setFrom(sender);
+        mmh.setSubject("Pedido confirmado! Código: " + obj.getId());
+        mmh.setSentDate(new Date(System.currentTimeMillis()));
+        mmh.setText(htmlFromTemplatePedido(obj),true);
+
+        return mimeMessage;
+    }
+
 }
--- src/main/java/com/lucasduarte/lojavirtual/service/EmailService.java
+++ src/main/java/com/lucasduarte/lojavirtual/service/EmailService.java
@@ -4,12 +4,18 @@ import com.lucasduarte.lojavirtual.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
+import javax.mail.internet.MimeMessage;
+
 
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
 
 
