ab1a2c08cde56a1dfe34b82dcfb0e28c1be5ac96
Lucas Duarte <lucas.inacio@corenmg.gov.br>
2020-07-11 17:48:38 -0300

    New smtp mail to Google

---
+++ src/main/java/com/lucasduarte/lojavirtual/config/ProdConfig.java
@@ -0,0 +1,40 @@
+package com.lucasduarte.lojavirtual.config;
+
+import com.lucasduarte.lojavirtual.service.DBService;
+import com.lucasduarte.lojavirtual.service.EmailService;
+import com.lucasduarte.lojavirtual.service.SmtpMailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+    @Autowired
+    private DBService service;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public Boolean instantiateDataBase() throws ParseException {
+        if(!"create".equals(strategy)){
+          return false;
+        }
+
+        service.instantiateTestDatabase();
+        return true;
+
+    }
+
+    @Bean
+    public EmailService emailService(){
+        return new SmtpMailService();
+    }
+
+}
